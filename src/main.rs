f9c9b8b6c363bebd145508ae4cf1a00cb7fcaf03
hang <balemaster@163.com>
2022-09-27 23:10:01 +0800

    remove unused

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,8 @@ fn main() {
     let arg = env::args().nth(1).expect("require a js file");
     let content = fs::read_to_string(arg).expect("read file failed");
     let mut lexer_ins = lexer::lexer::Lexer::new(&content);
-    let mut t = lexer_ins.advance();
-    println!("{:?}", t);
-    t = lexer_ins.advance();
-    println!("{:?}", t);
-    t = lexer_ins.advance();
+
+    // use advance method to get next token
+    let t = lexer_ins.advance();
     println!("{:?}", t);
 }
